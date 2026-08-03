0b7d3c2b7116ec0f69061e7d3cd96a8f4ad55685
Semih Bahadır <bnoktaersoz@gmail.com>
2026-08-04 00:21:35 +0300

    Changes to be committed : Tests PoiDomainTests Create Update Cases added

--- Tests/UnitTests/Domain/Services/PoiTests/PoiDomainServiceTest.cs
+++ Tests/UnitTests/Domain/Services/PoiTests/PoiDomainServiceTest.cs
@@ -6,43 +6,123 @@ using gis.Domain.Repositories;
 using gis.Domain.ResultPattern.Errors;
 using gis.Domain.Services;
 using NSubstitute;
+using Xunit.Abstractions;
 
-namespace Tests.UnitTests.Domain.PoiTests;
+namespace Tests.UnitTests.Domain.Services.PoiTests;
 
 public class PoiDomainServiceTest
 {
+    private readonly ITestOutputHelper _testOutputHelper;
     private readonly POIDomainService _poiService;
     private readonly IPointRepository _pointRepository;
     private readonly ITopologySuitePointContract _contract;
 
-    public PoiDomainServiceTest(POIDomainService poiService, IPointRepository pointRepository,
-        ITopologySuitePointContract contract)
+    public PoiDomainServiceTest(ITestOutputHelper testOutputHelper)
     {
+        _testOutputHelper = testOutputHelper;
         _pointRepository = Substitute.For<IPointRepository>();
         _contract = Substitute.For<ITopologySuitePointContract>();
         _poiService = new POIDomainService(_pointRepository, _contract);
     }
 
     [Fact]
-    public async void CreatePoi()
+    public async Task  CreatePoi_CoordExist_Case()
     {
-        var coords =  Coordinates.FromLatLon(40.7128, -74.0060,null).Value;
-        var name =  PointName.FromString("Mock Location").Value;
+
+        var lat = Latitude.Create(40.7128).Value;
+        var lon = Longitude.Create(40.7128).Value;
         var desc = PointDescription.FromString("Central Park").Value;
+        var name =  PointName.FromString("Mock Location").Value;
 
         // Koordinatın olmadığını (false) fakat ismin veritabanında olduğunu (true) simüle ediyoruz
-        _pointRepository.IsCoordinatesExistsAsync(coords).Returns(false);
+        _pointRepository.IsLatLonCoordinatesExistsAsync(lat,lon).Returns(false);
         _pointRepository.IsPointNameExistsAsync(name).Returns(true);
 
         // Act
-        var result = await _poiService.CreatePOIAggregate(coords, desc, name);
+        var result = await _poiService.CreatePoiAggregate(lat,lon, desc, name);
+
+        // Assert
+        _testOutputHelper.WriteLine(result.Error.ToString());
+        result.Error.Should().Be(RepositoryErrors.VALUE_ALREADY_EXIST_IN_DB);
+        result.IsFailure.Should().BeTrue();
+
+        await _pointRepository.Received(1).IsLatLonCoordinatesExistsAsync(lat,lon);
+        await _pointRepository.Received(1).IsPointNameExistsAsync(name);
+    } 
+[Fact]
+    public async Task  CreatePoi_Success_Case()
+    {
+
+        var lat = Latitude.Create(40.7128).Value;
+        var lon = Longitude.Create(40.7128).Value;
+        var desc = PointDescription.FromString("Central Park").Value;
+        var name =  PointName.FromString("Mock Location").Value;
+
+        _pointRepository.IsLatLonCoordinatesExistsAsync(lat,lon).Returns(false);
+        _pointRepository.IsPointNameExistsAsync(name).Returns(false);
+        _contract.CreateWktStringFromLatLon(lat, lon).Returns($"POINT ({lat.Value} {lon.Value})");
+
+        // Act
+        var result = await _poiService.CreatePoiAggregate(lat,lon, desc, name);
+        if (result.IsFailure)
+        {
+            _testOutputHelper.WriteLine(result.Error.Code);
+        }
+
+        
+        // Assert
+
+        _testOutputHelper.WriteLine(result.Value.Coordinates.Latitude.ToString());
+        _testOutputHelper.WriteLine(result.Value.Coordinates.Longitude.ToString());
+        _testOutputHelper.WriteLine(result.Value.Coordinates.WKT.Value);
+        result.IsSuccess.Should().BeTrue();
+    
+
+        await _pointRepository.Received(1).IsLatLonCoordinatesExistsAsync(lat,lon);
+        await _pointRepository.Received(1).IsPointNameExistsAsync(name);
+    }
+    [Fact]
+    public async Task CreatePoi_NameNull_Case()
+    {
+
+        var lat = Latitude.Create(40.7128).Value;
+        var lon = Longitude.Create(40.7128).Value;
+        var desc = PointDescription.FromString("Central Park").Value;
+        var name =  PointName.FromString("");
+
+        _pointRepository.IsLatLonCoordinatesExistsAsync(lat,lon).Returns(false);
+        _pointRepository.IsPointNameExistsAsync(name.Value).Returns(false);
+        _contract.CreateWktStringFromLatLon(lat, lon).Returns($"POINT ({lat.Value} {lon.Value})");
+
+        // Act
+        var result = await _poiService.CreatePoiAggregate(lat,lon, desc, name.Value);
 
         // Assert
         result.IsFailure.Should().BeTrue();
-        result.Error.Should().Be(RepositoryErrors.VALUE_ALREADY_EXIST_IN_DB);
-        
-        // Bu senaryoda iki metodun da birer kez çalıştırılmış olması gerekir
-        await _pointRepository.Received(1).IsCoordinatesExistsAsync(coords);
-        await _pointRepository.Received(1).IsPointNameExistsAsync(name);
+        result.Error.Should().Be(DomainErrors.POIErrors.WKT.INVALID_WKT_FORMAT);
+        _testOutputHelper.WriteLine(result.Error.ToString());
+
+        await _pointRepository.Received(1).IsPointNameExistsAsync(name.Value);
+        await _pointRepository.Received(1).IsLatLonCoordinatesExistsAsync(lat,lon);
     }
-}
+[Fact]
+    public async Task UpdatePoiAggregate()
+    {
+        var lat = Latitude.Create(40.7128).Value;
+        var lon = Longitude.Create(40.7128).Value;
+        var desc = PointDescription.FromString("Central Park").Value;
+        var name =  PointName.FromString("MockLocation").Value;
+        _pointRepository.IsLatLonCoordinatesExistsAsync(lat,lon).Returns(false);
+        _pointRepository.IsPointNameExistsAsync(name).Returns(false);
+
+        var result = await _poiService.CreatePoiAggregate(lat,lon, desc, name);
+        
+        _testOutputHelper.WriteLine(result.Value.Coordinates.Longitude.ToString());
+        _testOutputHelper.WriteLine(result.Value.Coordinates.Latitude.ToString());
+        _testOutputHelper.WriteLine(result.Value.Coordinates.WKT.ToString());
+        _testOutputHelper.WriteLine(result.Value.PointDesc.ToString());
+        _testOutputHelper.WriteLine(result.Value.PointName.ToString());
+        _testOutputHelper.WriteLine(result.Value.Status.ToString());
+        
+    }
+    }
